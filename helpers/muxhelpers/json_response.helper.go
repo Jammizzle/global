0b40e64e69c4dd2348498bee3957abe884da0b99
Peter Holt <peter.holt@dochq.co.uk>
2019-09-17 14:56:56 +0100

    Chnaged to export

--- helpers/muxhelpers/json_response.helper.go
+++ helpers/muxhelpers/json_response.helper.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-type jsonRes map[string]interface{}
+type JsonRes map[string]interface{}
 
-func jsonResponse(w http.ResponseWriter, status int, obj interface{}) error {
+func JsonResponse(w http.ResponseWriter, status int, obj interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(obj)
